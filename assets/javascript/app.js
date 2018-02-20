e0ebd473db1be5a0a172368daba0423231475cd3
Gavin Quirk <gavinquirk@gmail.com>
2018-02-20 15:15:36 -0800

    added buttons and timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,22 +12,20 @@ $(document).ready(function () {
             question: "What type of fighter does Luke Skywalker pilot?",
             choices: ["A-Wing", "B-Wing", "V-Wing", "X-Wing"],
             answer: "X-Wing"
+        },
+        {
+            question: "Who is Luke Skywalker's Father?",
+            choices: ["Han Solo", "Darth Vader", "Obi-Wan Kenobi", "Bob Skywalker"],
+            answer: "Darth Vader"
         }
     ]
-
-    // // Function for generating answer buttons
-    // function generateAnswers(array) {
-    //     console.log(array)
-    //     for (i = 0; i < array.length; i++) {
-    //         var newBtn = $("<input type='radio' value='array[i]'")
-    //         $("#test").append(newBtn)
-    //     }
-    // }
-
 
 
     // On start button click
     $("#startBtn").on("click", function () {
+
+        // var counter = questionArr.length * 5
+        var counter = 5
 
         // Hide and show containers
         $("#introContainer").hide()
@@ -43,27 +41,79 @@ $(document).ready(function () {
             var qConNew = $("<div id='qCon" + i + "' class='qCon'>")
             var cConNew = $("<div id='cCon" + i + "' class='cCon'>")
             var question = questionArr[i].question
-            var cCon = $("#cCon" + i + "")
-            var qCon = $("#qCon" + i + "")
-
+            var cCon = "#cCon" + i + ""
+            var qCon = "#qCon" + i + ""
             var text = "text"
             var choices = questionArr[i].choices
+            var newTestBtn = $("<input type='radio' name='testBtn' id='testBtn' value='testBtn'>")
+            var testBtn = "#testBtn"
+
+            // Variable Function for generating choice content
+            var choiceContent = function () {
+                for (c = 0; c < choices.length; c++) {
+                    var choiceBtn = $("<input type='radio' name='btnRow" +i+ "' id='"+choices[c]+"'>")
+
+                    $(cCon).append(choices[c], choiceBtn)
+
+
+
+                }
+            }
 
             // Create a div with rows and columns for each question
             gameContainer.append(rowNew.append(colNew.append(
-                qConNew.append(question),
+                qConNew,
                 cConNew
             )))
+            // Append question to question container
+            $(qCon).append(question)
+            // Append Choice Content
+            choiceContent()
 
-            // Generate choice buttons
-            for (i = 0; i < choices.length; i++) {
-                var choice = choices[i]
-                var choiceBtn = $("<input type='radio' value='choice'>")
-                // Append to choice container
-                $(cCon).append(choiceBtn, choice)
+        } // END OF FORLOOP
+
+        // ANSWER CHECK FUNCTION
+        $("#testArea").append(newTestBtn)
+
+            var answerCheck = function() {
+                if ($('#E-11 Blaster Rifle').is(':checked')) {
+
+                console.log("Test Button Clicked")
+                }
+                // if ($('#E-11 Blaster Rifle').is(':checked')) {
+
+                //     console.log("ANSWER CHECKED")
+                // }
+
             }
 
-        }
+
+        // Timer
+        var interval = setInterval(function () {
+            counter--
+            $("#counter").html("Time Left: " + counter)
+            if (counter <= 0) {
+                console.log("Time Up")
+                clearInterval(interval)
+                gameContainer.hide()
+                answerCheck()
+            }
+        }, 1000)
+
+        // var answerCheck = function() {
+        //     $("#radio_1, #radio_2", "#radio_3").change(function () {
+        //         if ($("#radio_1").is(":checked")) {
+        //             $('#div1').show();
+        //         }
+        //         else if ($("#radio_2").is(":checked")) {
+        //             $('#div2').show();
+        //         }
+        //         else 
+        //             $('#div3').show();
+        //     });        
+        // }
+
+
 
 
 
